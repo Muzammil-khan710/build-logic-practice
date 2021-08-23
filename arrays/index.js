9d7d52d695633a3324021c04779ab9b8b78ea156
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-23 21:54:40 +0530

    question-2 of Functions completed

--- arrays/index.js
+++ arrays/index.js
@@ -7,3 +7,17 @@ var sum = 0;
 for(var i in array) {
     sum = sum + array[i]
 }
+
+console.log(sum)
+
+// average of an array and display the output
+
+var array1 = [1,2,3,4,5,6]
+
+var sum1 = 0;
+
+for(var j in array1) {
+    sum1 = sum1 + array1[j]
+}
+const average = sum1/array1.length
+console.log(average)
