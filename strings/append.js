7760bff7863eb2b5b8dd4c4e0dda0a50395e1add
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-24 14:35:18 +0530

    question-2 of Strings completed

---
+++ strings/append.js
@@ -0,0 +1,13 @@
+// Write a program that reads two strings and append first string to the second
+
+let firstString = prompt("enter first string")
+
+let secondString = prompt("enter second string")
+
+const result = secondString + firstString;
+
+console.log(result)
+
+// we can also use concat method
+
+// const result = secondString.concat(firstString)
