e7af1c977b5d16c8aca40832a3d999763493ca8a
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-26 19:45:24 +0530

    updated functionality in question-5 of Arrays

--- arrays/sum.js
+++ arrays/sum.js
@@ -1,20 +1,15 @@
-var array1  = [3,5,2,9,4]
+let array1  = [3,5,2,9,4]
 
-var array2 = [6,2,8,1,3]
+let array2 = [6,2,8,1,3]
 
-var sum1 = 0;
+let sum1 = 0;
+let sum2 = 0
 
-for(var i in array1) {
+for(let i in array1 && array2) {
     sum1 = sum1 + array1[i]
-}
-
-var sum2 = 0;
-
-for(var i in array2) {
     sum2 = sum2 + array2[i]
 }
 
 const output = sum1 + sum2
 
 console.log(output)
-
