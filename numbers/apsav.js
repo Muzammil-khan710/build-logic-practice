cc6bac5abaf3a15793fa85de694d9916e9fb134f
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-27 23:25:19 +0530

    question-5 of Numbers completed

---
+++ numbers/apsav.js
@@ -0,0 +1,14 @@
+const side = Number(prompt("Enter side of a square")) 
+
+const area = side * side 
+
+const perimeter = 4 * side
+
+const surfaceArea = 6 * side * side
+
+const volume = side * side * side 
+
+console.log("area: ", area)
+console.log("perimeter: ", perimeter)
+console.log("Surface Area: ", surfaceArea)
+console.log("perimeter: ", volume)
