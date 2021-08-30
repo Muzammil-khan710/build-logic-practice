5d26fe5a0fe76b955b0a043987408e9a3729ac19
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-30 20:32:11 +0530

    question-2 of Functions completed

---
+++ functions/hexagon.js
@@ -0,0 +1,8 @@
+const side = Number(prompt("Enter the side of hexagon"))
+
+function areaOfHexagon() {
+    let area = ((3 * Math.sqrt(3))*(side*side)/2).toFixed[3];
+    return area
+}
+
+areaOfHexagon()
