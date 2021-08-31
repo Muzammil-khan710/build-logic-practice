ab72598a579ce9c4ce88a31b0e538a6cf6e6b368
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-31 21:40:45 +0530

    fixed syntax error in  question-2 of Functions

--- functions/hexagon.js
+++ functions/hexagon.js
@@ -1,7 +1,7 @@
 const side = Number(prompt("Enter the side of hexagon"))
 
 function areaOfHexagon() {
-    let area = ((3 * Math.sqrt(3))*(side*side)/2).toFixed[3];
+    let area = ((3 * Math.sqrt(3))*(side*side)/2).toFixed(3);
     return area
 }
 
