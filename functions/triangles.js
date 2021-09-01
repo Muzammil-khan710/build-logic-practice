06116456ace34f6e0d2c5d1f10129f011591dcef
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-09-01 20:37:59 +0530

    first logic in Question-1 of Objects completed

---
+++ functions/triangles.js
@@ -0,0 +1,32 @@
+const firstAngle = Number(prompt("Enter first angle "))
+
+const secondAngle = Number(prompt("Enter second angle"))
+
+const thirdAngle = Number(prompt("Enter third angle"))
+
+const sumOfAngles = firstAngle + secondAngle + thirdAngle;
+
+// if(sumOfAngles === 180) {
+//     equilateralTriangle();
+//     isoscelesTriangle();
+// }
+// scaleneTriangle()
+
+// function equilateralTriangle() {
+//     if(firstAngle == secondAngle == thirdAngle) {
+//         console.log("These angles forms an Equilateral triangle")
+//     }
+// }
+
+// function isoscelesTriangle() {
+//     if(firstAngle == secondAngle || secondAngle == thirdAngle) {
+//         console.log("These angles forms an Isosceles triangle")
+//     }
+// }
+
+// function scaleneTriangle() {
+//     if(firstAngle!= secondAngle != thirdAngle) {
+//         console.log("These angles forms an sccalen Trirangle")
+//     }
+// }
+
